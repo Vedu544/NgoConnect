2f3a09fce60091fbbcdb2f5e2e0c30101c63e55f
Vedant Kulkarni <85640004+Vedu544@users.noreply.github.com>
2024-10-01 10:53:51 +0530

    Update Popup.jsx

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'; 
+import { NavLink } from "react-router-dom"; 
 
-const Popup = () => {
+const Popup = ({ onClose }) => { 
+
+
   return (
-    <div>Popup</div>
+    <div className='fixed inset-0 flex justify-center items-center bg-black bg-opacity-50'>
+      <div className='bg-white p-9 rounded shadow-lg relative'>
+        <button onClick={onClose} className='absolute top-2 right-2'>
+          <FaTimes />
+        </button>
+        <h1 className='text-lg font-bold'>Choose a way to <br></br>
+          Create or Login account</h1>
+
+        <div className='mt-6 justify-center font-bold w-full'>
+          <NavLink to={"/NgoRegister"}
+          className='bg-green-500 text-white p-3 rounded block w-full text-center'>NGO Member
+          </NavLink>
+        </div>
+
+        <div className='mt-4 flex justify-center font-bold'> 
+          <NavLink to={"/UserRegister"}
+           className='bg-green-500 text-white p-2 rounded w-full'>User
+          </NavLink>
+        </div>
+      </div>
+    </div>
   )
 }
 
